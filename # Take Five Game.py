50e2c78f012a92d6bff3d56561cb8e74d65ef82a
John Wilson III <johnwilsoniii@macbook-pro-7.lan>
2020-06-17 01:45:19 -0700

    at the point of placing cards

--- # Take Five Game.py
+++ # Take Five Game.py
@@ -2,21 +2,21 @@
 import random
 
 # Objects
-class Deck():
-    def __init__(self):
-        self.deck = {1: 1, 2: 1, 3: 1, 4: 1, 5: 2, 6: 1, 7: 1, 8: 1, 9: 1, 10: 3, 11: 5, 12: 1, 13: 1, 14: 1, 15: 2, 16: 1, 17: 1, 18: 1, 19: 1, 20: 3, 21: 1, 22: 5, 23: 1, 24: 1, 25: 2, 26: 1, 27: 1, 28: 1, 29: 1, 30: 3, 31: 1, 32: 1, 33: 5, 34: 1, 35: 2, 36: 1, 37: 1, 38: 1, 39: 1, 40: 3, 41: 1, 42: 1, 43: 1, 44: 5, 45: 3, 46: 1, 47: 1, 48: 1, 49: 1, 50: 3, 51: 1, 52: 1, 53: 1, 54: 1, 55: 7, 56: 1, 57: 1, 58: 1, 59: 1, 60: 3, 61: 1, 62: 1, 63: 1, 64: 1, 65: 2, 66: 5, 67: 1, 68: 1, 69: 1, 70: 3, 71: 1, 72: 1, 73: 1, 74: 1, 75: 2, 76: 1, 77: 5, 78: 1, 79: 1, 80: 3, 81: 1, 82: 1, 83: 1, 84: 1, 85: 2, 86: 1, 87: 1, 88: 5, 89: 1, 90: 3, 91: 1, 92: 1, 93: 1, 94: 1, 95: 2, 96: 1, 97: 1, 98: 1, 99: 5, 100: 3, 101: 1, 102: 1, 103: 1, 104: 1}
+class Deck(): 
+    def __init__(self): 
+        self.deck = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 2), (6, 1), (7, 1), (8, 1), (9, 1), (10, 3), (11, 5), (12, 1), (13, 1), (14, 1), (15, 2), (16, 1), (17, 1), (18, 1), (19, 1), (20, 3), (21, 1), (22, 5), (23, 1), (24, 1), (25, 2), (26, 1), (27, 1), (28, 1), (29, 1), (30, 3), (31, 1), (32, 1), (33, 5), (34, 1), (35, 2), (36, 1), (37, 1), (38, 1), (39, 1), (40, 3), (41, 1), (42, 1), (43, 1), (44, 5), (45, 3), (46, 1), (47, 1), (48, 1), (49, 1), (50, 3), (51, 1), (52, 1), (53, 1), (54, 1), (55, 7), (56, 1), (57, 1), (58, 1), (59, 1), (60, 3), (61, 1), (62, 1), (63, 1), (64, 1), (65, 2), (66, 5), (67, 1), (68, 1), (69, 1), (70, 3), (71, 1), (72, 1), (73, 1), (74, 1), (75, 2), (76, 1), (77, 5), (78, 1), (79, 1), (80, 3), (81, 1), (82, 1), (83, 1), (84, 1), (85, 2), (86, 1), (87, 1), (88, 5), (89, 1), (90, 3), (91, 1), (92, 1), (93, 1), (94, 1), (95, 2), (96, 1), (97, 1), (98, 1), (99, 5), (100, 3), (101, 1), (102, 1), (103, 1), (104, 1)]
     def __str__(self):
         return str(self.deck)
     def deal_cards(self, deck, players):
         for pl in players.items():
             for c in random.sample(list(self.deck), 10):
-                    pl[1].hand[c] = self.deck[c]
-                    del self.deck[c]
-            print("Player #{}'s hand = {}" .format(pl[0], list(pl[1].hand.items())))
+                    pl[1].hand.insert(0,c)
+                    self.deck.remove(c)
+            print("Player #{}'s hand = {}" .format(pl[0], list(pl[1].hand)))
              
 class Player():
     def __init__(self):
-        self.hand = {}
+        self.hand = []
         self.points = 0
         self.rounds_won = 0
         self.games_won = 0
@@ -25,30 +25,37 @@ class Player():
 
 class Board():
     def __init__(self):
-        self.turn_rec = {}                                      # Dictionary log of {turn number : {player : card played}}
-        self.board_row1 = ['   ','   ','   ','   ','   ']
-        self.board_row2 = ['   ','   ','   ','   ','   ']
-        self.board_row3 = ['   ','   ','   ','   ','   ']
-        self.board_row4 = ['   ','   ','   ','   ','   ']
+        self.turn_rec = {}      # Dictionary log of {turn number : {player : {card_key:card_value}}, ...}
+        self.board = {1:[], 2:[], 3:[], 4:[]}
     def show(self):
-        print(self.board_row1[1] + ' | ' + self.board_row1[2] + ' | ' + self.board_row1[3] + ' | ' + self.board_row1[4] + '\n--------------------\n' + self.board_row2[1] + ' | ' + self.board_row2[2] + ' | ' + self.board_row2[3] + ' | ' + self.board_row2[4] + '\n--------------------\n' + self.board_row3[1] + ' | ' + self.board_row3[2] + ' | ' + self.board_row3[3] + ' | ' + self.board_row3[4] + '\n--------------------\n' + self.board_row4[1] + ' | ' + self.board_row4[2] + ' | ' + self.board_row4[3] + ' | ' + self.board_row4[4])
+        for row in self.board.values():
+            print(row)
     def place_cards(self, players): 
         self.turn_rec[len(self.turn_rec.items())+1] = {}
-        for pl in players.items():                                                          # cycle through players and add choices to the above line as key pairs
-            print("Player #{}'s hand = {}" .format(pl[0], list(pl[1].hand.items())))
-            ch_card_key = input('Enter the number of the card to play ')
-            self.turn_rec[len(self.turn_rec.items())][pl[0]] = ch_card_key
-        print(list(self.turn_rec.items()))
+        for pl in players.items():                                                  # cycle through players and add choices to the above line as key pairs
+            print("\n Player #{}'s hand = {}" .format(pl[0], list(pl[1].hand)))
+            c_card_indx = int(input('Enter the index location of the card to play \n'))
+            self.turn_rec[len(self.turn_rec.items())][pl[0]] = {pl[1].hand[c_card_indx][0]: pl[1].hand[c_card_indx][1]}
+            del pl[1].hand[c_card_indx]
+        print(self.turn_rec[len(self.turn_rec.items())])
+        staged_cards = [sc[0] for sc in sorted([list(c.items())[0:1] for c in [s_c for s_c in [p_c[1] for p_c in dict.items(self.turn_rec[len(self.turn_rec.items())])]]])]       #sorted list of chosen cards by key as tuples [(k:v), ...]
+        for card in staged_cards:
+            if 0 == {len(self.board[1]), len(self.board[2]), len(self.board[3]), len(self.board[4])}:
+                print('empty row!')
+            ch_row = int(input('Enter row number for {} to be placed' .format(card)))
+            
 
-class Game():
+class Monitor():
     def __init__(self):
         self.round_count = 0
+        self.deck_after_round = {}
 
 deck = Deck()
-players = {pl: Player() for pl in range(1,int(input('how many people are playing?'))+1)}    #Dictionary of 2 - 10 {player numbers : player objects}
+players = {pl: Player() for pl in range(1,int(input('how many people are playing?'))+1)}        #Dictionary of 2 - 10 {player numbers : player objects}
 board = Board()
 deck.deal_cards(deck, players)
 board.show()
-board.place_cards(players)
-
-# Testing Git
+while len(board.turn_rec) < 10:
+    board.place_cards(players)
+    board.show()
+    continue
